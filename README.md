4d1d6513112d7ab902caa0dd73b99dd878f169c3
jgomezz <jgomezz@gmail.com>
2025-05-04 22:29:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Laboratorio Calificado 2
+
+
+#### 1.- Listar al ménos 8 docentes del departamento
+
+![Image Description](./images/teacher_list_screen.png)
+```
+```
+
+
--- apple_lab08_pract02_UIKit/TeacherController.swift
+++ apple_lab08_pract02_UIKit/TeacherController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TeacherController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Teacher List"
     }
 
 
