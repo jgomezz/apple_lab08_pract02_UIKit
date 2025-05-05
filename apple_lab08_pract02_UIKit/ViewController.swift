eb517c4d0fa739bae4c67cff7e49988f5e501136
jgomezz <jgomezz@gmail.com>
2025-05-04 21:46:12 -0500

    Initial Commit

---
+++ apple_lab08_pract02_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple_lab08_pract02_UIKit
+//
+//  Created by Jaime Gomez on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
